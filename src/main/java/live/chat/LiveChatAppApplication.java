b1ac156054296a20f0401caaae931343b1f3e7a9
arun <you@example.com>
2023-12-10 22:03:04 +0530

    make it better

--- src/main/java/live/chat/LiveChatAppApplication.java
+++ src/main/java/live/chat/LiveChatAppApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class LiveChatAppApplication {
 
+	
 	public static void main(String[] args) {
 		SpringApplication.run(LiveChatAppApplication.class, args);
 	}
